907ed6bbade02c31c1f980c8638a1e89b7cce1b2
Rudessss <152311496+Rudessss@users.noreply.github.com>
2023-12-01 11:16:15 +0700

    Update home.js

--- home.js
+++ home.js
@@ -14,12 +14,12 @@ document.addEventListener("DOMContentLoaded", function () {
     document.getElementById("greeting").innerText = greeting;
 });
 
-var counter = 1;
-setInterval(function(){
-    document.getElementById('radio' + counter).checked = true;
-    counter++;
-    if(counter>4) counter = 1;
-}, 5000);
+// var counter = 1;
+// setInterval(function(){
+//     document.getElementById('radio' + counter).checked = true;
+//     counter++;
+//     if(counter>4) counter = 1;
+// }, 5000);
 
 // Curently Showing
 const wrapers = document.querySelectorAll(".wraper");
@@ -65,3 +65,48 @@ document.addEventListener("DOMContentLoaded", function () {
         }
     });
 });
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    createGrid(9, 9);
+});
+
+function createGrid(rows, cols) {
+    const grid = document.getElementById("grid");
+
+    for (let i = 0; i < rows; i++) {
+        const row = grid.insertRow(i);
+
+        for (let j = 0; j < cols; j++) {
+            const cell = row.insertCell(j);
+            cell.addEventListener("click", function () {
+                toggleCellSelection(this);
+            });
+
+            // Add preselected class to specific cells
+            if ((i + 1 === 1 && (j + 1 === 1 || j + 1 === 3)) || (i + 1 === 2 && j + 1 === 6)) {
+                cell.classList.add("preselected");
+                cell.removeEventListener("click", function () {
+                    toggleCellSelection(this);
+                });
+            }
+        }
+    }
+}
+
+function toggleCellSelection(cell) {
+    if (!cell.classList.contains("preselected")) {
+        cell.classList.toggle("selected");
+    }
+}
+
+function toggleSelectedToPreselected() {
+    const selectedCells = document.querySelectorAll(".selected");
+    const numberOfSelectedCells = selectedCells.length;
+
+    selectedCells.forEach(cell => {
+        cell.classList.remove("selected");
+        cell.classList.add("preselected");
+    });
+    alert(`Number of selected cells: ${numberOfSelectedCells}`);
+}
