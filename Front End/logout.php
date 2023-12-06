081623b09aadc03960728ecdc24e6d36af81e0de
Gnuga <152195652+Gnuga@users.noreply.github.com>
2023-12-07 03:43:49 +0700

    Update logout.php

--- Front End/logout.php
+++ Front End/logout.php
@@ -1,13 +1,11 @@
 <?php
-session_start(); // Start the session
+session_start(); 
 
-// Unset all session variables
 $_SESSION = array();
 
-// Destroy the session
 session_destroy();
 
-// Redirect to the login page or any other page after logout
+
 header('Location: home.php');
 exit();
 ?>
