7ca9e17890c62275b187dc8fbd71787cea3e5959
Gnuga <152195652+Gnuga@users.noreply.github.com>
2023-12-07 03:44:31 +0700

    Update upcoming.php

--- Front End/upcoming.php
+++ Front End/upcoming.php
@@ -26,16 +26,6 @@ if (isset($_SESSION['user_logged_in']) && $_SESSION['user_logged_in'] === true) 
 </head>
 
 <body>
-    <!-- <header>
-        <div id="logo">BEEFLIX</div>
-        <nav>
-            <a href="home.php">Home</a>
-            <a href="upcoming.html">Upcoming</a>
-            <a href="register.php">Register</a>
-            <a href="login.php">Login</a>
-        </nav>
-        <div class="profile-icon"><a href="#"><i class="fa fa-user"></i></a></div>
-    </header> -->
     
     <div class="container">
         <div id="slide">
